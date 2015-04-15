d39ab2404ffdc53021d6882e405e14821047e6c3
Peter Zion <peter.zion@gmail.com>
2015-04-15 16:41:15 -0400

    Add FTL classes for scope timing

---
+++ Ticks.h
@@ -0,0 +1,70 @@
+/*
+ *  Copyright 2010-2015 Fabric Software Inc. All rights reserved.
+ */
+
+#ifndef _FTL_Ticks_h
+#define _FTL_Ticks_h
+
+#include <FTL/Config.h>
+
+#include <stdint.h>
+#if defined(FTL_OS_LINUX)
+# include <time.h>
+# include <sys/time.h>
+#elif defined(FTL_OS_DARWIN)
+# include <mach/mach_time.h>
+#elif defined(FTL_OS_WINDOWS)
+# include <windows.h>
+#else
+# error "Unsupported OS"
+#endif
+
+namespace FTL
+{
+  inline uint64_t GetCurrentTicks()
+  {
+#if defined(FTL_OS_WINDOWS)
+    LARGE_INTEGER counts;
+    ::QueryPerformanceCounter( &counts );
+    return uint64_t( counts.QuadPart );
+#elif defined(FTL_OS_LINUX)
+    struct timespec tv;
+    clock_gettime( CLOCK_MONOTONIC, &tv );
+    return uint64_t(tv.tv_sec) * uint64_t(1000000000) + uint64_t(tv.tv_nsec);
+#elif defined(FTL_OS_DARWIN)
+    return mach_absolute_time();
+#else
+# error "Unsupported OS"
+#endif
+  }
+
+  inline double GetSecondsForTicksDiff( uint64_t ticksDiff )
+  {
+#if defined(FTL_OS_WINDOWS)
+    static double secondsPerTick = 0.0;
+    if ( !secondsPerTick )
+    {
+      LARGE_INTEGER ticksPerSecond;
+      ::QueryPerformanceFrequency( &ticksPerSecond );
+      secondsPerTick = 1.0 / double(ticksPerSecond.QuadPart);
+    }
+    return double(ticksDiff) * secondsPerTick;
+#elif defined(FTL_OS_LINUX)
+    return double(ticksDiff) * 1e-9;
+#elif defined(FTL_OS_DARWIN)
+    static mach_timebase_info_data_t sTimebaseInfo;
+    if ( !sTimebaseInfo.denom )
+      mach_timebase_info(&sTimebaseInfo);
+    return double(ticksDiff) * double(sTimebaseInfo.numer) * 1e-9 / double(sTimebaseInfo.denom);
+#else
+# error "Unsupported OS"
+#endif
+  }
+
+  inline double GetSecondsBetweenTicks( uint64_t start, uint64_t end )
+  {
+    return GetSecondsForTicksDiff( end - start );
+  }
+}
+
+#endif //_FTL_Ticks_h
---
+++ Timer.h
@@ -0,0 +1,59 @@
+/*
+ *  Copyright 2010-2015 Fabric Software Inc. All rights reserved.
+ */
+
+#ifndef _FTL_Timer_h
+#define _FTL_Timer_h
+
+#include <FTL/Ticks.h>
+
+#include <stdio.h>
+
+namespace FTL
+{
+  class Timer
+  {
+  public:
+
+    Timer( char const *desc )
+      : m_desc( desc )
+      , m_offset( 0.0 )
+      , m_begin( GetCurrentTicks() )
+    {
+    }
+
+    ~Timer()
+    {
+      printf( "%s %0.3lfms\n", m_desc, getElapsedMS() );
+    }
+
+    double getElapsedMS( bool reset = false )
+    {
+      uint64_t currentTicks = GetCurrentTicks();
+      double elapsed =
+        m_offset + GetSecondsBetweenTicks( m_begin, currentTicks ) * 1e3;
+      if ( reset )
+        m_begin = currentTicks;
+      return elapsed;
+    }
+
+    void pause()
+    {
+      m_offset +=
+        GetSecondsBetweenTicks( m_begin, GetCurrentTicks() ) * 1e3;
+    }
+
+    void resume()
+    {
+      m_begin = GetCurrentTicks();
+    }
+
+  private:
+
+    char const *m_desc;
+    double m_offset;
+    uint64_t m_begin;
+  };
+}
+
+#endif //_FTL_Timer_h
