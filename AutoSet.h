331ac0b018d5f4b0c2d1f22270e7485922967583
Helge Mathee <helge.mathee@fabricengine.com>
2015-09-14 16:10:38 +0200

    FE-5248: use autoset for evaluation flags

---
+++ AutoSet.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2010-2015 Fabric Software Inc. All rights reserved.
+ */
+
+#ifndef _FTL_AutoSet_h
+#define _FTL_AutoSet_h
+
+#include <FTL/Config.h>
+
+FTL_NAMESPACE_BEGIN
+
+template< class T > class AutoSet
+{
+public:
+
+  AutoSet( T &lValue, T newRValue )
+    : m_lValue( lValue )
+    , m_oldRValue( lValue )
+  {
+    m_lValue = newRValue;
+  }
+
+  ~AutoSet()
+  {
+    m_lValue = m_oldRValue;
+  }
+
+private:
+
+  T &m_lValue;
+  T m_oldRValue;
+};
+
+FTL_NAMESPACE_END
+
+#endif //_FTL_AutoSet_h
