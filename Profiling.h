7f3d31e38e07f0f9a8abdb0d07e4f0e23ef77c08
Helge Mathee <helge.mathee@fabricengine.com>
2016-11-07 15:53:13 +0100

    FTL Profiling first pass

--- Profiling.h
+++ Profiling.h
@@ -5,8 +5,8 @@
 #pragma once
 
 #include <FTL/Ticks.h>
-#include <map>
 #include <vector>
+#include <map>
 
 FTL_NAMESPACE_BEGIN
 
@@ -18,6 +18,7 @@ public:
     : m_label(label)
     , m_parentIndex(parentIndex)
     , m_begin( GetCurrentTicks() )
+    , m_elapsed( 0.0 )
   {
     m_end = m_begin;
   }
@@ -27,6 +28,7 @@ public:
     , m_parentIndex(-1)
     , m_begin(0)
     , m_end(0)
+    , m_elapsed(0.0)
   {
   }
 
@@ -35,23 +37,41 @@ public:
     , m_parentIndex(other.m_parentIndex)
     , m_begin(other.m_begin)
     , m_end(other.m_end)
+    , m_elapsed(other.m_elapsed)
   {
   }
 
   double getSeconds() const
   {
-    return GetSecondsBetweenTicks(m_begin, m_end);
+    return m_elapsed + GetSecondsBetweenTicks(m_begin, m_end);
   }
 
   double getMS() const
   {
-    return getSeconds() * 1000.0;
+    return m_elapsed + getSeconds() * 1000.0;
   }
 
   char const *m_label;
   int m_parentIndex;
   uint64_t m_begin;
   uint64_t m_end;
+  double m_elapsed;
+};
+
+struct ProfilingEntry
+{
+  ProfilingEntry(char const * label)
+  : m_label(label)
+  , m_level(0)
+  , m_invocations(0)
+  , m_seconds(0.0)
+  {
+  }
+
+  char const * m_label;
+  unsigned int m_level;
+  unsigned int m_invocations;
+  double m_seconds;
 };
 
 class ProfilingStack
@@ -74,7 +94,9 @@ public:
 
     int parentIndex = -1;
     if( m_stackEnd > 0 )
+    {
       parentIndex = m_parentKeyStack[m_stackEnd-1];
+    }
 
     if(m_numProfiles == m_profilingInfos.size())
       m_profilingInfos.resize(m_profilingInfos.size() * 2);
@@ -103,9 +125,89 @@ public:
     m_profilingInfos[index].m_end = endTick;
     m_stackEnd--;
 
-    printf("%s duration %fms\n", m_profilingInfos[index].m_label, (float)m_profilingInfos[index].getMS());
-
     return true;
+  }
+
+  bool stopProfilingEvent(int index)
+  {
+    uint64_t endTick = GetCurrentTicks();
+    if( index < 0 )
+      return false;
+    if( index >= m_numProfiles )
+      return false;
+
+    m_profilingInfos[index].m_elapsed += GetSecondsBetweenTicks(m_profilingInfos[index].m_begin, endTick);
+    m_profilingInfos[index].m_begin = m_profilingInfos[index].m_end = endTick;
+    return true;
+  }
+
+  bool resumeProfilingEvent(int index)
+  {
+    uint64_t beginTick = GetCurrentTicks();
+    if( index < 0 )
+      return false;
+    if( index >= m_numProfiles )
+      return false;
+    m_profilingInfos[index].m_begin = m_profilingInfos[index].m_end = beginTick;
+    return true;
+  }
+
+  std::vector< ProfilingEntry > getReportEntries() const
+  {
+    std::vector< ProfilingEntry > entries;
+    std::map< std::string, unsigned int > lookup;
+
+    for(size_t i=0;i<m_numProfiles;i++)
+    {
+      std::string key = m_profilingInfos[i].m_label;
+      int index = m_profilingInfos[i].m_parentIndex;
+      while(index >= 0)
+      {
+        key += m_profilingInfos[index].m_label;
+        index = m_profilingInfos[index].m_parentIndex;
+      }
+      
+      std::map< std::string, unsigned int >::const_iterator it = lookup.find(key.c_str());
+      if(it == lookup.end())
+      {
+        ProfilingEntry entry(m_profilingInfos[i].m_label);
+        entry.m_invocations = 1;
+        entry.m_level = 0;
+        int index = m_profilingInfos[i].m_parentIndex;
+        while(index >= 0)
+        {
+          entry.m_level++;
+          index = m_profilingInfos[index].m_parentIndex;
+        }
+
+        entry.m_seconds = m_profilingInfos[i].getSeconds();
+        lookup.insert(std::pair< std::string, unsigned int >(key.c_str(), (unsigned int)entries.size()));
+        entries.push_back(entry);
+      }
+      else
+      {
+        entries[it->second].m_seconds += m_profilingInfos[i].getSeconds();
+        entries[it->second].m_invocations++;
+      }
+    }
+
+    return entries;
+  }
+
+  std::string getReportString() const
+  {
+    std::string result;
+    std::vector< ProfilingEntry > entries = getReportEntries();
+    std::string indent;
+    for(size_t i=0;i<entries.size();i++)
+    {
+      char buf[1024];
+      if(indent.length() != entries[i].m_level * 2)
+        indent.assign(entries[i].m_level * 2, ' ');
+      snprintf(buf, 1024, "%s[%s] called %d times, %g milliseconds.\n", indent.c_str(), entries[i].m_label, entries[i].m_invocations, entries[i].m_seconds * 1000.0);
+      result += buf;
+    }
+    return result;
   }
 
 private:
@@ -117,19 +219,29 @@ private:
   unsigned int m_stackEnd;
 };
 
-class AutoProfilingBracket
+class AutoProfilingEvent
 {
 public:
 
-  AutoProfilingBracket(char const * label, ProfilingStack & stack)
+  AutoProfilingEvent(char const * label, ProfilingStack & stack)
     : m_stack(stack)
   {
     m_index = stack.beginProfilingEvent(label);
   }
 
-  ~AutoProfilingBracket()
+  ~AutoProfilingEvent()
   {
     m_stack.endProfilingEvent(m_index);
+  }
+
+  void stop()
+  {
+    m_stack.stopProfilingEvent(m_index);
+  }
+
+  void resume()
+  {
+    m_stack.resumeProfilingEvent(m_index);
   }
 
 private:
